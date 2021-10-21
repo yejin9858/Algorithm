410b68c155c0e9d124da0688c1fdacd0e9e34f7d
YEJIN <yejon9858@gmail.com>
2021-10-21 13:34:52 +0900

    [BOJ 1748] 수 이어 쓰기 1

---
+++ BruteForce/1748.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	
+	int N, sum = 0;
+	cin >> N;
+	/*
+	//방법1
+	int i = 0;
+	for (int j = 0;; i++) {
+		if (N < 9 * pow(10, i) + j)
+			break;
+		j = 9 * pow(10, i);
+	}
+	for (int j = 1; j <= i; j++) {
+		sum += j * 9 * pow(10, j-1);
+	}
+	sum += (N - pow(10, i) + 1)* (i+1);
+	*/
+
+	//방법 2
+	for (int i = 1; i <= N; i *= 10) {
+		sum += N - i + 1;
+	}
+
+	cout << sum;
+}
+
