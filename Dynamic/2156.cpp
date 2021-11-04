0817261190e4ddfd94018ac07bbdd7caba47d651
YEJIN <yejon9858@gmail.com>
2021-11-04 23:48:25 +0900

    [BOJ 2156] 포도주 시식

---
+++ Dynamic/2156.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int d[10001];
+int a[10001];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+
+	d[1] = a[1];
+	d[2] = d[1] + a[2];
+
+	for (int i = 3; i <= n; i++) {
+		d[i] = max(d[i - 3] + a[i - 1] + a[i], max(d[i - 1], d[i - 2] + a[i]));
+	}
+
+	cout << d[n];
+}
